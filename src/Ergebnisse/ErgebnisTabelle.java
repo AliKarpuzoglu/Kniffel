18a6fbac1268c12f3ab0a0f85c6600c57143e661
Ali-Karpuzoglu <al.karpuzoglu@gmail.com>
2015-05-17 18:40:35 +0200

    finished the constructor for Ergebnistabelle

--- src/Ergebnisse/ErgebnisTabelle.java
+++ src/Ergebnisse/ErgebnisTabelle.java
@@ -3,12 +3,12 @@ package Ergebnisse;
 import java.util.*;
 
 /**TODO : Change "ergebnis" to "ergebnisse"?
- * 
+ *
  */
 public class ErgebnisTabelle {
 
     /**
-     * 
+     *
      */
     public ErgebnisTabelle() {
         for (int i = 1; i <= 6; i++) {
@@ -16,76 +16,84 @@ public class ErgebnisTabelle {
         }
         ergebnis[6] = new Dreierpasch();
         ergebnis[7] = new Viererpash();
+        ergebnis[8] = new Quadratfolge();
+        ergebnis[9] = new FullHouse();
+        ergebnis[10] = new KleineStrasse();
+        ergebnis[11] = new GrosseStrasse();
+        ergebnis[12] = new Kniffel();
+        ergebnis[13] = new Chance();
 
     }
 
     /**
-     * 
+     *
      */
     private Ergebnis[] ergebnis;
 
     /**
-     * 
+     *
      */
     private final int BONUS = 35;
 
     /**
-     * 
+     *
      */
     private int summeOben;
 
     /**
-     * 
+     *
      */
     private int summeUnten;
 
     /**
-     * 
+     *
      */
     private int summeGesamt;
 
     /**
-     * 
+     *
      */
     public void tabelleAnzeigen() {
-        // TODO implement here
+        // TosTring
     }
 
     /**
-     * 
+     *
      */
     public void gesamtSumme() {
-        // TODO implement here
+        for (Ergebnis e :ergebnis ) {
+
+        }
     }
 
     /**
-     * 
+     *
      */
     public void toString() {
         // TODO implement here
     }
 
     /**
-     * 
+     *
      */
     public void ergebnisEintragen(Ergebnis ergebnis) {
         // TODO implement here
     }
 
     /**
-     * 
+     *
      */
     public void berechneOben() {
         // TODO implement here
     }
 
     /**
-     * 
+     *
      */
     public void berechneUnten() {
         // TODO implement here
     }
-    
+
     public Ergebnis[] getErgebnis(){
         return ergebnis;
     }
