5c01246e4fe9a0a41c5def2e473e4dce30f67306
kshits <katharina_s15@hotmail.de>
2015-05-25 14:57:26 +0200

    added actionlistener

---
+++ src/gui/GameListener.java
@@ -0,0 +1,44 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class GameListener extends JFrame implements ActionListener {
+	JButton button1;
+	JButton button2;
+	JLabel label;
+	JPanel panel;
+
+	public GameListener() {
+		this.setTitle("Kniffel");
+		this.setSize(400, 200);
+
+		panel = new JPanel();
+		label = new JLabel();
+
+		button1 = new JButton("Spielen");
+		button2 = new JButton("Abbrechen");
+
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+
+		panel.add(button1);
+		panel.add(button2);
+
+		panel.add(label);
+		this.add(panel);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent bt) {
+		if (bt.getSource() == this.button1) {
+			label.setText("Spiel startet jetzt!");
+
+		} else if (bt.getSource() == this.button2) {
+			System.exit(0);
+		}
+
+	}
+
+}
