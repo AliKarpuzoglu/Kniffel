ce0b0307802d1f3aa674fcdeedab5493e940bbb5
kshits <katharina_s15@hotmail.de>
2015-05-25 13:53:57 +0200

    added gui

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,17 +1,69 @@
 package gui;
 
+import java.util.Scanner;
+import ergebnisse.ErgebnisTabelle;
 import logic.Spieler;
 import logic.Wurf;
 
-/**
- * 
- */
 public class GUI {
+	String[] spieler = new String[6];
+	int i = 0, r = 0;
 
-	public GUI() {
-	    }
+	public void printReihenfolge(Spieler[] spieler) {
+		System.out.println("Die Reihenfolge der Spieler ist:");
+		for (int i = 0; i < spieler.length; i++) {
+			System.out.println((i + 1) + ": " + spieler[i].toString());
+		}
+	}
+
+	public void printSpielerNamen() {
+
+		for (int z = 0; z < spieler.length; z++) {
+			System.out.println("" + spieler[z]);
+		}
+
+	}
+
+	public void printComputerSpielerDialog(int i) {
+		Scanner computer = new Scanner(System.in);
+		System.out.printf("Wie viele Computerspieler sollen teilnehmen?");
+		int anzahlComp = computer.nextInt();
+		for (int tre = 0; tre <= anzahlComp; tre++) {
+			spieler[i] = new String("Computer Spieler");
+			System.out.println("" + spieler[i]);
+		}
+		computer.close();
+
+	}
+
+	public void printSpielerdialog() {
+
+		Scanner scanner = new Scanner(System.in);
+		Scanner anzahl = new Scanner(System.in);
+		System.out.println("Es duerfen bis zu 6 Spieler teilnehmen");
+		System.out
+				.println("Bitte geben sie die Anzahl der menschlichen Spieler an(1-6)");
+		r = anzahl.nextInt();
+
+		while (i <= r) {
+			System.out
+					.printf("Bitte geben sie den Namen des naechsten Spielers ein.");
+			spieler[i] = scanner.next();
+			i++;
+		}
+		scanner.close();
+		anzahl.close();
+
+	}
+
+	public void printWelcome() {
+		System.out.println("Willkommen zu Kniffel");
+		System.out
+				.println("Die Gruppe um Jonas Jaeckel wuenscht ihnen ein erfreuliches Spiel");
+	}
 
 	public void printWurf(Wurf wurf) {
+		System.out.println(wurf.toString());
 
 	}
 
@@ -19,7 +71,18 @@ public class GUI {
 	 * @param spieler
 	 */
 	public void printErgebnisspalte(Spieler spieler) {
-		// TODO implement here
-	}
+		System.out
+				.println("|   Spieler    | 1 | 2 | 3 | 4 | 5 | 6 |Summe|Bonus|Dreier|Vierer|Quadrat|FullHouse|KleineStrasse|"
+						+ "|GrosseStrasse|Kniffel|Chance|Summe|Gesamt");
 
+		System.out.println("| " + spieler + " | " + ergebnisse[0] + " | "
+				+ ergebnisse[1] + " | " + ergebnisse[2] + " | " + ergebnisse[3]
+				+ " | " + ergebnisse[4] + " | " + ergebnisse[5] + " | "
+				+ getSummeOben() + " | " + getBonus() + " | " + ergebnisse[6]
+				+ " | " + ergebnisse[7] + " | " + ergebnisse[8] + " | "
+				+ ergebnisse[9] + " | " + ergebnisse[10] + "" + ergebnisse[11]
+				+ " | " + ergebnisse[12] + " | " + ergebnisse[13] + " | "
+				+ getSummeUnten() + " | " + getGesamtSumme());
+
+	}
 }
